54034c087678ca4d6da3f80db19e778c28cb8ab5
Evgeny Mazyar <evgen.mazyar@gmail.com>
2025-02-06 23:03:15 +0400

    style: improve layout for mobile devices

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -1,9 +1,7 @@
 import { Button } from "@/components/Button";
 
 export const AddButton: React.FC<{ onClick: () => void }> = ({ onClick }) => (
-	<Button className="ml-6" variant="withIcon" onClick={onClick} title="Создать семинар" aria-label="Создать семинар">
-		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-			<path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-		</svg>
+	<Button variant="outline" className="m-6" onClick={onClick} title="Создать семинар" aria-label="Создать семинар">
+		Создать семинар
 	</Button>
 );
--- src/components/SeminarItem.tsx
+++ src/components/SeminarItem.tsx
@@ -7,30 +7,39 @@ type SeminarItemProps = ISeminar & { setActiveModal: (type: ISeminarAction) => v
 
 export const SeminarItem = ({ id, photo, title, description, date, time, setActiveModal }: SeminarItemProps) => {
 	return (
-		<li key={id} className="relative grid grid-cols-[auto_1fr_auto_auto] grid-rows-none items-center justify-items-start gap-x-6 p-6 hover:bg-blue-50 rounded-sm">
-			<div className="relative col-span-1">
-				<img
-					alt=""
-					src={photo || placeholder}
-					onError={(e) => e.currentTarget.src = placeholder}
-					className="size-12 flex-none rounded-xs"
-				/>
+		<li key={id} >
+			<div className="relative p-6 rounded-sm hover:bg-blue-50 
+			flex flex-col items-start flex-[1_1_auto] 
+			md:grid md:grid-rows-[1fr_auto] md:justify-start 
+			lg:grid lg:grid-rows-1 lg:grid-cols-[auto_1fr_auto_auto] lg:items-center lg:justify-start gap-6">
+				<div className="relative col-span-1 ">
+					<img
+						alt=""
+						src={photo || placeholder}
+						onError={(e) => e.currentTarget.src = placeholder}
+						className="sm:size-full md:size-60 lg:size-20 flex-none rounded-xs"
+					/>
+				</div>
+
+				<div className="flex flex-col col-start-2 col-end-3 justify-center items-start">
+					<p className="text-sm wrap font-semibold text-gray-900 ">{title}</p>
+					<p className="mt-1 text-xs text-gray-500 text-wrap text-left lg:line-clamp-2 lg:truncate sm:line-clamp-none sm:truncate-none max-w-md">{description}</p>
+				</div>
+
+				<div className="
+			flex w-40 order-4
+			md:gap-6 md:col-start-1 md:col-end-4 md:justify-between
+			lg:gap-2 lg:col-start-3 lg:col-end-4 lg:justify-center lg:row-start-1 lg:row-end-2">
+					<DeleteButton onClick={() => setActiveModal({ seminarId: id, action: CrudType.DELETE })} />
+					<EditButton onClick={() => setActiveModal({ seminarId: id, action: CrudType.EDIT })} />
+				</div>
+
+				<div className="flex flex-col md:text-right sm:text-left md:col-start-3 md:col-end-4 lg:col-start-4 lg:col-end-5 sm:order-3">
+					<time className="text-sm text-gray-900">{date}</time>
+					<time className="mt-1 text-xs text-gray-500">{time}</time>
+				</div>
 			</div>
 
-			<div className="flex flex-col col-start-2 col-end-3 justify-center items-start">
-				<p className="text-sm wrap font-semibold text-gray-900 ">{title}</p>
-				<p className="mt-1 text-xs text-gray-500 text-wrap text-left line-clamp-2 truncate max-w-md">{description}</p>
-			</div>
-
-			<div className="flex gap-2 col-start-3 col-end-4 w-48">
-				<DeleteButton onClick={() => setActiveModal({ seminarId: id, action: CrudType.DELETE })} />
-				<EditButton onClick={() => setActiveModal({ seminarId: id, action: CrudType.EDIT })} />
-			</div>
-
-			<div className="flex flex-col text-right col-start-4 col-end-5">
-				<time className="text-sm text-gray-900">{date}</time>
-				<time className="mt-1 text-xs text-gray-500">{time}</time>
-			</div>
 		</li>
 
 	)
